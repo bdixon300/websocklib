aa873b5b961126cfe4a55fbab65d42ed11d07b26
Ben Dixon <ben.dixon300@gmail.com>
2026-04-19 09:06:20 -0700

    Add http payload handshake content (still need to integrate tcp client and the webframe serialization)

--- src/websocketclient.cpp
+++ src/websocketclient.cpp
@@ -1,9 +1,64 @@
 #include "websocketclient.hpp"
+#include <sstream>    
+#include <openssl/rand.h>
+#include <openssl/bio.h>
+#include <openssl/evp.h> 
 
 namespace websocklib {
 
-WebSocketClient::WebSocketClient() {}
+constexpr char PLACEHOLDER_ORIGIN[] = "https://placeholderorigin.com";
+
+WebSocketClient::WebSocketClient(const std::string& hostUrl): m_hostUrl(hostUrl) {}
 
 WebSocketClient::~WebSocketClient() {}
+
+void WebSocketClient::connect()
+{
+    sendHandshake();
+}
+
+void WebSocketClient::sendHandshake()
+{
+    // // Establish TCP Connection
+    // m_tcpClient.tcpConnect();
+
+    // // Send HTTP websocket upgrade handshake
+    // m_tcpClient.sendMessage(generateHandShakePayload());
+}
+
+std::string WebSocketClient::generateHandShakePayload()
+{
+    std::ostringstream ss;
+
+    ss << "GET /chat HTTP/1.1 "
+       << "Host: " << m_hostUrl << " "
+       << "Upgrade: websocket "
+       << "Connection: Upgrade"
+       << "Sec-WebSocket-Key: " << generateWebSocketKey() << " "
+       << "Origin: " << PLACEHOLDER_ORIGIN
+       << "Sec-WebSocket-Protocol: chat, superchat "
+       << "Sec-WebSocket-Version: 13";
+
+    return ss.str();
+}
+
+std::string WebSocketClient::generateWebSocketKey() {
+    unsigned char random_bytes[16];
+    RAND_bytes(random_bytes, sizeof(random_bytes));
+
+    // Base64 encode
+    BIO* b64 = BIO_new(BIO_f_base64());
+    BIO* mem = BIO_new(BIO_s_mem());
+    BIO_push(b64, mem);
+    BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
+    BIO_write(b64, random_bytes, sizeof(random_bytes));
+    BIO_flush(b64);
+
+    char* data;
+    long len = BIO_get_mem_data(mem, &data);
+    std::string key(data, len);
+    BIO_free_all(b64);
+    return key;
+}
 
 } // namespace websocklib
--- src/websocketclient.hpp
+++ src/websocketclient.hpp
@@ -4,62 +4,37 @@
 
 namespace websocklib {
 
-/**
- * 
- * The websocket will have a connect/disconnect etc
- * 
- * It will also hold a 
- * 1. tcp client instance
- * 2. webframe serializer (to serialize / deserialize data from/to websocket frames into raw bytes to send over wire)
- * 
- * Once we start the TCP connection/TCP client
- * we need to send a HTTP GET request to upgrade our tcp connection to websocket
- * we get a response back that we need to handle -> https://datatracker.ietf.org/doc/html/rfc6455#section-4.1
- * 
- * eg:
- * 
- *   * GET /chat HTTP/1.1\r\n
-        Host: example.com\r\n
-        Upgrade: websocket\r\n
-        Connection: Upgrade\r\n
-        Sec-WebSocket-Key: <base64 encoded 16 random bytes>\r\n
-        Sec-WebSocket-Version: 13\r\n
-        \r\n
-    
-        Once done, We should receive a response as follows ->
-
-        HTTP/1.1 101 Switching Protocols\r\n
-        Upgrade: websocket\r\n
-        Connection: Upgrade\r\n
-        Sec-WebSocket-Accept: <base64 encoded SHA-1 hash>\r\n
-        \r\n
- * 
- * 
- * 
- * After that all packets from tcp client are websocket frames and can be parsed as such
- * 
- * 
- */
-
 class WebSocketClient {
 public:
-    WebSocketClient();
-
+    WebSocketClient(const std::string& hostUrl);
 
     ~WebSocketClient();
 
-private:
+    // establish websocket connection, 
+    // (starts tcp connection, generates and sends handshake payload, 
+    // receives/processes websocket frames after that)
+    void connect();
 
-    
+private:
     // Method will generate the upgrade GET request to leverage our underlying
     // TCP connection as a web socket connection
     std::string generateHandShakePayload();
 
+    // Helper method to generate a unique web socket key when sending a HTTP websocket handshake
+    std::string generateWebSocketKey();
+
     // sends handshake to TCP (HTTP payload required)
     void sendHandshake();
 
+    // URL to connect to using the websocket protocol
+    std::string m_hostUrl;
 
-    // TODO - something to process the webframes and do something with them
+    // TCP Client for reading / writing payloads using web socket protocol
+    // TODO - Implement/integrate the TCP client here
+    //TCPClient m_tcpClient;
+
+    // TODO - use webframe serializer to decode the bytes into readable format
+    // WebFrameSerializer m_webFrameSerializer
 };
 
 } // namespace websocklib
